4d4777a52a0b57e5e86f124a00937dbf42ec6cab
Daniel24356 <igwedaniel128@gmail.com>
2025-09-23 15:36:56 -0700

    uncommented teacher.jsx

--- src/pages/Teacher/teacher.jsx
+++ src/pages/Teacher/teacher.jsx
@@ -1,72 +1,72 @@
-// import "./teacher.css"
-// import teach1 from "../../assets/teach-1.jpg"
-// import teach2 from "../../assets/teach-2.jpg"
-// import teach3 from "../../assets/teach-3.jpg"
-// import teach4 from "../../assets/teach-4.jpg"
-// import bulb1 from "../../assets/bulb-1.png"
-// import { IoShareSocialOutline } from "react-icons/io5";
-// import { GoArrowRight } from "react-icons/go";
+import "./teacher.css"
+import teach1 from "../../assets/teach-1.jpg"
+import teach2 from "../../assets/teach-2.jpg"
+import teach3 from "../../assets/teach-3.jpg"
+import teach4 from "../../assets/teach-4.jpg"
+import bulb1 from "../../assets/bulb-1.png"
+import { IoShareSocialOutline } from "react-icons/io5";
+import { GoArrowRight } from "react-icons/go";
 
-// const Teacher = () => {
-//     return(
-//         <>
-//         <section className="teach-sec">
-//             <div>
-//             <div className="bulbs-div">
-//                <img src={bulb1} alt="" />
-//                 <p>Instructor</p>
-//             </div>
-//             <div className="prof-divs">
-//             <div className="prof-ins">
-//                 <h1>Our Professional Instructor</h1>
-//                 <p>You'll find something to spark your curiosity and enhance</p>
-//             </div>
-//             <button>
-//               View All Teacher 
-//               <GoArrowRight className="go-icons"/>
-//               </button>
-//             </div>
-//             </div>
+const Teacher = () => {
+    return(
+        <>
+        <section className="teach-sec">
+            <div>
+            <div className="bulbs-div">
+               <img src={bulb1} alt="" />
+                <p>Instructor</p>
+            </div>
+            <div className="prof-divs">
+            <div className="prof-ins">
+                <h1>Our Professional Instructor</h1>
+                <p>You'll find something to spark your curiosity and enhance</p>
+            </div>
+            <button>
+              View All Teacher 
+              <GoArrowRight className="go-icons"/>
+              </button>
+            </div>
+            </div>
           
-//           <div className="teacher-div">
-//           <div className="teacher-box">
-//             <div className="share-div">
-//               <IoShareSocialOutline/>
-//             </div>
-//             <img src={teach1} alt="" />
-//             <h1>Dana White</h1>
-//             <p>UI/UX Exparet</p>
-//           </div>
-//           <div className="teacher-box">
-//           <div className="share-div">
-//               <IoShareSocialOutline/>
-//             </div>
-//             <img src={teach2} alt="" />
-//             <h1>Dana White</h1>
-//             <p>UI/UX Exparet</p>
-//           </div>
-//           <div className="teacher-box">
-//           <div className="share-div">
-//               <IoShareSocialOutline/>
-//             </div>
-//             <img src={teach3} alt="" />
-//             <h1>Dana White</h1>
-//             <p>UI/UX Exparet</p>
-//           </div>
-//           <div className="teacher-box">
-//           <div className="share-div">
-//               <IoShareSocialOutline/>
-//             </div>
-//             <img src={teach4} alt="" />
-//             <h1>Dana White</h1>
-//             <p>UI/UX Exparet</p>
-//           </div>
-//           </div>
+          <div className="teacher-div">
+          <div className="teacher-box">
+            <div className="share-div">
+              <IoShareSocialOutline/>
+            </div>
+            <img src={teach1} alt="" />
+            <h1>Dana White</h1>
+            <p>UI/UX Exparet</p>
+          </div>
+          <div className="teacher-box">
+          <div className="share-div">
+              <IoShareSocialOutline/>
+            </div>
+            <img src={teach2} alt="" />
+            <h1>Dana White</h1>
+            <p>UI/UX Exparet</p>
+          </div>
+          <div className="teacher-box">
+          <div className="share-div">
+              <IoShareSocialOutline/>
+            </div>
+            <img src={teach3} alt="" />
+            <h1>Dana White</h1>
+            <p>UI/UX Exparet</p>
+          </div>
+          <div className="teacher-box">
+          <div className="share-div">
+              <IoShareSocialOutline/>
+            </div>
+            <img src={teach4} alt="" />
+            <h1>Dana White</h1>
+            <p>UI/UX Exparet</p>
+          </div>
+          </div>
          
 
-//         </section>
-//         </>
-//     )
-// }
+        </section>
+        </>
+    )
+}
 
-// export default Teacher
+export default Teacher
