94faa3f8a40918f6277d200210e7f9c143cb930c
Daniel24356 <igwedaniel128@gmail.com>
2025-09-23 15:28:49 -0700

    commented know.jsx

--- src/pages/Know/know.jsx
+++ src/pages/Know/know.jsx
@@ -1,120 +1,120 @@
-import './know.css'
-import edu1 from "../../assets/edu1.svg"
-import edu2 from "../../assets/edu2.svg"
-import edu3 from "../../assets/edu3.svg"
-import edu4 from "../../assets/edu4.svg"
-import edu5 from "../../assets/edu5.svg"
-import edu6 from "../../assets/edu6.svg"
-import girlp from "../../assets/girl-phone.jpg"
-import girlb from "../../assets/girl-button.png"
-import blueb from "../../assets/icon-star.svg"
-import purp1 from "../../assets/purp-btn1.png"
-import purp2 from "../../assets/purp-btn2.png"
-import prof1 from "../../assets/profile-cir.png"
-import { HiOutlineLightBulb } from 'react-icons/hi2'
-import { IoMdPlay } from "react-icons/io";
+// import './know.css'
+// import edu1 from "../../assets/edu1.svg"
+// import edu2 from "../../assets/edu2.svg"
+// import edu3 from "../../assets/edu3.svg"
+// import edu4 from "../../assets/edu4.svg"
+// import edu5 from "../../assets/edu5.svg"
+// import edu6 from "../../assets/edu6.svg"
+// import girlp from "../../assets/girl-phone.jpg"
+// import girlb from "../../assets/girl-button.png"
+// import blueb from "../../assets/icon-star.svg"
+// import purp1 from "../../assets/purp-btn1.png"
+// import purp2 from "../../assets/purp-btn2.png"
+// import prof1 from "../../assets/profile-cir.png"
+// import { HiOutlineLightBulb } from 'react-icons/hi2'
+// import { IoMdPlay } from "react-icons/io";
 
-const Know = () => {
-    return (
-        <>
-            <section className='know-sec'>
-                <div className='trust-div'>
-                    <div className='trust-by'>
-                        <p>Trusted by:</p>
-                    </div>
-                    <div className='edu-div'>
-                        <div>
-                            <img src={edu1} alt="" />
-                        </div>
-                        <div>
-                            <img src={edu2} alt="" />
-                        </div>
-                        <div>
-                            <img src={edu3} alt="" />
-                        </div>
-                        <div>
-                            <img src={edu4} alt="" />
-                        </div>
-                        <div>
-                            <img src={edu5} alt="" />
-                        </div>
-                        <div>
-                            <img src={edu6} alt="" />
-                        </div>
-                    </div>
+// const Know = () => {
+//     return (
+//         <>
+//             <section className='know-sec'>
+//                 <div className='trust-div'>
+//                     <div className='trust-by'>
+//                         <p>Trusted by:</p>
+//                     </div>
+//                     <div className='edu-div'>
+//                         <div>
+//                             <img src={edu1} alt="" />
+//                         </div>
+//                         <div>
+//                             <img src={edu2} alt="" />
+//                         </div>
+//                         <div>
+//                             <img src={edu3} alt="" />
+//                         </div>
+//                         <div>
+//                             <img src={edu4} alt="" />
+//                         </div>
+//                         <div>
+//                             <img src={edu5} alt="" />
+//                         </div>
+//                         <div>
+//                             <img src={edu6} alt="" />
+//                         </div>
+//                     </div>
 
-                </div>
+//                 </div>
 
-                <section className='study-sec'>
-                    <div className='headphone'>
-                        <div className='positive-box'>
-                            <div className='positive-div'>
-                                <div className='pos-rev'>
-                                    <h1>2.4k</h1>
-                                    <p>Positive Review</p>
-                                </div>
-                                <div className='blueb'>
-                                    <img src={blueb} alt="" />
-                                </div>
-                            </div>
-                            <div className='girlp'>
-                                <img src={girlp} alt="" />
-                            </div>
-                        </div>
-                        <div className='girlb'>
-                            {/* <img src={girlb} alt="" /> */}
-                            <div className='play-btn'>
-                                <IoMdPlay className='play-icon'/>
-                            </div>
-                        </div>
-                    </div>
+//                 <section className='study-sec'>
+//                     <div className='headphone'>
+//                         <div className='positive-box'>
+//                             <div className='positive-div'>
+//                                 <div className='pos-rev'>
+//                                     <h1>2.4k</h1>
+//                                     <p>Positive Review</p>
+//                                 </div>
+//                                 <div className='blueb'>
+//                                     <img src={blueb} alt="" />
+//                                 </div>
+//                             </div>
+//                             <div className='girlp'>
+//                                 <img src={girlp} alt="" />
+//                             </div>
+//                         </div>
+//                         <div className='girlb'>
+//                             {/* <img src={girlb} alt="" /> */}
+//                             <div className='play-btn'>
+//                                 <IoMdPlay className='play-icon'/>
+//                             </div>
+//                         </div>
+//                     </div>
 
-                    <div>
-                        <div className='gate-div'>
-                            <HiOutlineLightBulb className='bulb-icons' />
-                            <p>Gateway to Lifelong Learning</p>
-                        </div>
-                        <div className='studyhub'>
-                            <h1>Know Studyhub Empowering</h1>
-                            <h1>Learners Worldwide</h1>
-                        </div>
-                        <div className='passionate'>
-                            <p>We are passionate about education and dedicated to providing high-</p>
-                            <p>quality learning resources for learners of all backgrounds.</p>
-                        </div>
-                        <div className='learn-me'>
-                            <div className='prof-box'>
-                                <img src={purp1} alt="" />
-                                <div>
-                                    <p className='exp-txt'>Learn with Expert</p>
-                                    <p className='pass-txt'>We are passionate education.</p>
-                                </div>
-                            </div>
-                            <div className='prof-box'>
-                                <img src={purp2} alt="" />
-                                <div>
-                                    <p className='exp-txt'>Learn with Expert</p>
-                                    <p className='pass-txt'>We are passionate education.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cir-p'>
-                            <div className='prof-cir'>
-                                <div>
-                                    <img src={prof1} alt="" />
-                                </div>
-                                <div className='ap-name'>
-                                    <p className='exp-txt'>William James</p>
-                                    <p className='pass-txt'>CEO, Studyhub Online Education</p>
-                                </div>
-                            </div>
-                            <button>About Us</button>
-                        </div>
-                    </div>
-                </section>
-            </section>
-        </>
-    )
-}
+//                     <div>
+//                         <div className='gate-div'>
+//                             <HiOutlineLightBulb className='bulb-icons' />
+//                             <p>Gateway to Lifelong Learning</p>
+//                         </div>
+//                         <div className='studyhub'>
+//                             <h1>Know Studyhub Empowering</h1>
+//                             <h1>Learners Worldwide</h1>
+//                         </div>
+//                         <div className='passionate'>
+//                             <p>We are passionate about education and dedicated to providing high-</p>
+//                             <p>quality learning resources for learners of all backgrounds.</p>
+//                         </div>
+//                         <div className='learn-me'>
+//                             <div className='prof-box'>
+//                                 <img src={purp1} alt="" />
+//                                 <div>
+//                                     <p className='exp-txt'>Learn with Expert</p>
+//                                     <p className='pass-txt'>We are passionate education.</p>
+//                                 </div>
+//                             </div>
+//                             <div className='prof-box'>
+//                                 <img src={purp2} alt="" />
+//                                 <div>
+//                                     <p className='exp-txt'>Learn with Expert</p>
+//                                     <p className='pass-txt'>We are passionate education.</p>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div className='cir-p'>
+//                             <div className='prof-cir'>
+//                                 <div>
+//                                     <img src={prof1} alt="" />
+//                                 </div>
+//                                 <div className='ap-name'>
+//                                     <p className='exp-txt'>William James</p>
+//                                     <p className='pass-txt'>CEO, Studyhub Online Education</p>
+//                                 </div>
+//                             </div>
+//                             <button>About Us</button>
+//                         </div>
+//                     </div>
+//                 </section>
+//             </section>
+//         </>
+//     )
+// }
 
-export default Know
+// export default Know
