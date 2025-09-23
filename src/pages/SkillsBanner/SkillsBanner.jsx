23ebdf483d2509f23f8509505fa4c1a20ff8b2dc
Daniel24356 <igwedaniel128@gmail.com>
2025-09-23 15:31:19 -0700

    uncommented skillBanner.jsx

--- src/pages/SkillsBanner/SkillsBanner.jsx
+++ src/pages/SkillsBanner/SkillsBanner.jsx
@@ -1,26 +1,26 @@
-// import "./SkillsBanner.css"
-// import woman1 from "../../assets/women-1.png"
-// import { GoArrowRight } from "react-icons/go";
+import "./SkillsBanner.css"
+import woman1 from "../../assets/women-1.png"
+import { GoArrowRight } from "react-icons/go";
 
-// const SkillsBanner = () => {
-//     return(
-//         <>
-//         <section className="skillbanner-sec">
-//             <img src={woman1} alt="" />
-//             <div className="skl-div">
-//             <div>
-//                 <h1>Skills Certificate From</h1>
-//                 <h1>the Studyhub</h1>
-//             </div>
-//             <button>
-//                 View All Course
-//                 <GoArrowRight className="go-icons"/>
-//                  </button>
-//             </div>
+const SkillsBanner = () => {
+    return(
+        <>
+        <section className="skillbanner-sec">
+            <img src={woman1} alt="" />
+            <div className="skl-div">
+            <div>
+                <h1>Skills Certificate From</h1>
+                <h1>the Studyhub</h1>
+            </div>
+            <button>
+                View All Course
+                <GoArrowRight className="go-icons"/>
+                 </button>
+            </div>
             
-//         </section>
-//         </>
-//     )
-// }
+        </section>
+        </>
+    )
+}
 
-// export default SkillsBanner
+export default SkillsBanner
