f7db064dc4111ac877ec80893492e2d9505e60c9
Daniel24356 <igwedaniel128@gmail.com>
2025-09-23 15:26:23 -0700

    commented home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,31 +1,31 @@
-import Article from "../Article/article"
-import Category from "../Category/category"
-import Count from "../Count/count"
-import Featured from "../Featured/featured"
-import Footer from "../Footer/footer"
-import Header from "../Header/header"
-import HeroSection from "../Herosection/Herosection"
-import Know from "../Know/know"
-import SkillsBanner from "../SkillsBanner/SkillsBanner"
-import Study from "../Study/study"
-import Teacher from "../Teacher/teacher"
+// import Article from "../Article/article"
+// import Category from "../Category/category"
+// import Count from "../Count/count"
+// import Featured from "../Featured/featured"
+// import Footer from "../Footer/footer"
+// import Header from "../Header/header"
+// import HeroSection from "../Herosection/Herosection"
+// import Know from "../Know/know"
+// import SkillsBanner from "../SkillsBanner/SkillsBanner"
+// import Study from "../Study/study"
+// import Teacher from "../Teacher/teacher"
 
-const Home = () => {
-    return (
-        <>
-        <Header/>
-        <HeroSection/>
-        <Know/>
-        <Category/>
-        <Featured/>
-        <Study/>
-        <Teacher/>
-        <Count/>
-        <Article/>
-        <SkillsBanner/>
-        <Footer/>
-        </>
-    )
-}
+// const Home = () => {
+//     return (
+//         <>
+//         <Header/>
+//         <HeroSection/>
+//         <Know/>
+//         <Category/>
+//         <Featured/>
+//         <Study/>
+//         <Teacher/>
+//         <Count/>
+//         <Article/>
+//         <SkillsBanner/>
+//         <Footer/>
+//         </>
+//     )
+// }
 
-export default Home
+// export default Home
