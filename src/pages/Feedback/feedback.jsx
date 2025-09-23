3761b8fd89e8f085518f4f535de101c6f0555482
Daniel24356 <igwedaniel128@gmail.com>
2025-09-23 15:14:08 -0700

    uncommented feedback.jsx

--- src/pages/Feedback/feedback.jsx
+++ src/pages/Feedback/feedback.jsx
@@ -1,42 +1,42 @@
 
 
-// const Feedback = () => {
-//     return(
-//         <>
-//         <section>
-//         <div>
-//             {/* icons */}
-//             <p></p>
-//         </div>
-//         <h1>Our Students Feedback</h1>
-//         <p>You'll find something to spark your curiosity and enhance</p>
+const Feedback = () => {
+    return(
+        <>
+        <section>
+        <div>
+            {/* icons */}
+            <p></p>
+        </div>
+        <h1>Our Students Feedback</h1>
+        <p>You'll find something to spark your curiosity and enhance</p>
 
-//         <div>
-//             <img src="" alt="" />
-//             <div>
-//                 {/* icons */}
-//                 <div>
-//                     <p>Contrary to popular belief, Lorem Ipsum is not</p>
-//                     <p>simply random text. It has roots in a piece of  </p>
-//                     <p>classical Latin literature from 45 BC, making it</p>
-//                     <p>over 2000 years old.</p>
-//                 </div>
-//                 <div>
-//                     <img src="" alt="" />
-//                 </div>
-//                 <div>
-//                     <h3>Emma Elizabeth</h3>
-//                     <p>Assistant Teacher</p>
-//                 </div>
-//                 <div>
-//                     {/* icons */}
-//                 </div>
-//             </div>
-//         </div>
-//         </section>
+        <div>
+            <img src="" alt="" />
+            <div>
+                {/* icons */}
+                <div>
+                    <p>Contrary to popular belief, Lorem Ipsum is not</p>
+                    <p>simply random text. It has roots in a piece of  </p>
+                    <p>classical Latin literature from 45 BC, making it</p>
+                    <p>over 2000 years old.</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+                <div>
+                    <h3>Emma Elizabeth</h3>
+                    <p>Assistant Teacher</p>
+                </div>
+                <div>
+                    {/* icons */}
+                </div>
+            </div>
+        </div>
+        </section>
        
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
-// export default Feedback
+export default Feedback
